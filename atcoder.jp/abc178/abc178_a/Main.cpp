6a72178fe8db6d901d092bfbc29ce257fe933622
polyester <shunfreed2016@gmail.com>
2020-09-13 21:00:59 +0900

    [AC] abc178 abc178_a

---
+++ atcoder.jp/abc178/abc178_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  cout << (n ^ 1) << endl;
+  return 0;
+}
