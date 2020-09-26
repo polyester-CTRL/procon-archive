dbc56aa85f1c2537340e7f22fede0224ef51c8ec
polyester <shunfreed2016@gmail.com>
2020-09-26 21:01:14 +0900

    [AC] abl abl_a

---
+++ atcoder.jp/abl/abl_a/Main.cpp
@@ -0,0 +1,25 @@
+#include "atcoder/all"
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace atcoder;
+using namespace std;
+using ll = long long;
+int main() {
+  int k;
+  cin >> k;
+  for(int i = 0; i < k; i++) {
+    cout << "ACL";
+  }
+  cout << endl;
+  return 0;
+}
