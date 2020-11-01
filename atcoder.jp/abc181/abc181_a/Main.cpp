b4087328c787592711d00a31bf44c695debeb0af
polyester <shunfreed2016@gmail.com>
2020-11-01 21:00:55 +0900

    [AC] abc181 abc181_a

---
+++ atcoder.jp/abc181/abc181_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  if(n % 2 == 0) {
+    cout << "White\n";
+  } else {
+    cout << "Black\n";
+  }
+  return 0;
+}
