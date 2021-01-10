87990cbb3f388f96d38aa468f68ada04c3c65e58
polyester <shunfreed2016@gmail.com>
2021-01-10 21:20:15 +0900

    [AC] abc188 abc188_a

---
+++ atcoder.jp/abc188/abc188_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int x, y;
+  cin >> x >> y;
+  if(max(x, y) - min(x, y) < 3) {
+    cout << "Yes\n";
+  } else {
+    cout << "No\n";
+  }
+  return 0;
+}
