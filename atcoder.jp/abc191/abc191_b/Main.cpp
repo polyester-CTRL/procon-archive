415a527bcfdfa876bdbabd46c1971861c9cbdd44
polyester <shunfreed2016@gmail.com>
2021-02-06 21:06:30 +0900

    [AC] abc191 abc191_b

---
+++ atcoder.jp/abc191/abc191_b/Main.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  ll x;
+  cin >> n >> x;
+  vector<ll> a(n);
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  for(int i = 0; i < n; i++) {
+    if(a[i] == x) {
+      continue;
+    }
+    cout << a[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
