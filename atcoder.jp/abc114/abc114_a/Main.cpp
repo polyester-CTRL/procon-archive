dfcfb5795bea7f55b81a773ecb56bd43c2fc3019
polyester <shunfreed2016@gmail.com>
2021-02-17 14:19:54 +0900

    [AC] abc114 abc114_a

---
+++ atcoder.jp/abc114/abc114_a/Main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int x;
+  int ans = 0;
+  cin >> x;
+  if(x == 7 || x == 5 || x == 3) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+  return 0;
+}
