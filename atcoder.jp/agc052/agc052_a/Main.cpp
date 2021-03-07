1e2ff65c575c269d3e1e8f14597a430c0037d21b
polyester <shunfreed2016@gmail.com>
2021-03-08 00:14:36 +0900

    [AC] agc052 agc052_a

---
+++ atcoder.jp/agc052/agc052_a/Main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    for(int i = 0; i < n; i++) {
+      cout << 1;
+    }
+    for(int i = 0; i < n; i++) {
+      cout << 0;
+    }
+    cout << 1 << '\n';
+  }
+  return 0;
+}
