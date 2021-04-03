c24cba0fa4c3074392fe4e6657fe3783c5d3c3e0
polyester <shunfreed2016@gmail.com>
2021-04-03 23:26:31 +0900

    [AC] arc039 arc039_b

---
+++ atcoder.jp/arc039/arc039_b/Main.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+const ll MOD = 1000000007;
+
+// 引用元
+// https://drken1215.hatenablog.com/entry/2018/06/08/210000
+const int MAX = 510000;
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+int main() {
+  ll n, k;
+  cin >> n >> k;
+  COMinit();
+  if(n <= k) {
+    k = k % n;
+    cout << COM(n, k) << endl;
+  } else {
+    cout << COM(n + k - 1, k) << endl;
+  }
+  return 0;
+}
