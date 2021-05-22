46a50bfbbb5de80353382c279445624ab3820e01
polyester <shunfreed2016@gmail.com>
2021-05-22 21:01:05 +0900

    [AC] abc202 abc202_a

---
+++ atcoder.jp/abc202/abc202_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << 21 - a - b - c << endl;
+  return 0;
+}
