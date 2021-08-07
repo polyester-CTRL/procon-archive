c827ca41c235f0e882bdd533a307db4fac0eb057
polyester <shunfreed2016@gmail.com>
2021-08-07 16:14:31 +0900

    [AC] ahc005 ahc005_a

---
+++ atcoder.jp/ahc005/ahc005_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n, sx, sy;
+  cin >> n >> sx >> sy;
+  vector<string> c(n);
+  for(int i = 0; i < n; i++) {
+    cin >> c[i];
+  }
+  cout << endl;
+  return 0;
+}
