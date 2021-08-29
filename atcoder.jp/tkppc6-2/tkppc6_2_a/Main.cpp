b9e55703e4e3d79dc469141e6cc8b8a4ea13013a
polyester <shunfreed2016@gmail.com>
2021-08-29 16:39:49 +0900

    [AC] tkppc6-2 tkppc6_2_a

---
+++ atcoder.jp/tkppc6-2/tkppc6_2_a/Main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+const ll MOD = 998244353;
+ll modpow(ll x, ll n) {
+  ll ans = 1;
+  while(n > 0) {
+    if(n % 2 == 1) {
+      ans *= x;
+      ans %= MOD;
+    }
+    x *= x;
+    x %= MOD;
+    n >>= 1;
+  }
+  return ans;
+}
+int main() {
+  ll n;
+  cin >> n;
+  cout << modpow(2, n - 1) << endl;
+  return 0;
+}
