4f7e5e76f098ebe5842b86b724801f4f1bc625ae
polyester <shunfreed2016@gmail.com>
2021-09-29 14:30:30 +0900

    [AC] abc213 abc213_a

---
+++ atcoder.jp/abc213/abc213_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << (a ^ b) << endl;
+  return 0;
+}
