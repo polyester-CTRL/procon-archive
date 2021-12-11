5ea114c0561e17cf10a81db51dcc77df69658406
polyester <shunfreed2016@gmail.com>
2021-12-11 21:07:34 +0900

    [AC] abc231 abc231_b

---
+++ atcoder.jp/abc231/abc231_b/Main.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  map<string, int> mp;
+  for(int i = 0; i < n; i++) {
+    string tmp;
+    cin >> tmp;
+    mp[tmp]++;
+  }
+  int maxi = 0;
+  for(auto p : mp) {
+    maxi = max(maxi, p.second);
+  }
+  for(auto p : mp) {
+    if(p.second == maxi) {
+      cout << p.first << endl;
+      return 0;
+    }
+  }
+
+  return 0;
+}
