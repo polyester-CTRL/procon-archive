b0127adbc038d5f496b1614bd18402c6764ffe17
polyester <shunfreed2016@gmail.com>
2021-12-11 21:03:40 +0900

    [AC] abc231 abc231_a

---
+++ atcoder.jp/abc231/abc231_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  double x;
+  cin >> x;
+  printf("%.4f\n", x / 100.0);
+  return 0;
+}
