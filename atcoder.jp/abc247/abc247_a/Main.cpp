65b4913dc702b52ff12851683a508a7b3670f904
polyester <shunfreed2016@gmail.com>
2022-04-10 21:02:02 +0900

    [AC] abc247 abc247_a

---
+++ atcoder.jp/abc247/abc247_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  string s;
+  cin >> s;
+  cout << "0" << s.substr(0, 3) << endl;
+  return 0;
+}
