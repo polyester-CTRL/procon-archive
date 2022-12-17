0abb267eb5e0f204e8d655e26be798805e69cc04
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2022-12-17 21:01:13 +0900

    [AC] abc282 abc282_a

---
+++ atcoder.jp/abc282/abc282_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    cout << (char)(i + 'A');
+  }
+  cout << endl;
+  return 0;
+}
