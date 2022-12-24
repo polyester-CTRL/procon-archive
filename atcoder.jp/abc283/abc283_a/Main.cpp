8f897fb9468d8509c56ced261fed13b23f02ca19
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2022-12-24 21:02:21 +0900

    [AC] abc283 abc283_a

---
+++ atcoder.jp/abc283/abc283_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  ll a, b;
+  cin >> a >> b;
+  cout << (ll)pow(a, b) << endl;
+  return 0;
+}
