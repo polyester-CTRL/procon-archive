c8d574ff4a311c1259a019d4d82f0f62ad38edb3
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2023-05-20 21:01:14 +0900

    [AC] abc302 abc302_a

---
+++ atcoder.jp/abc302/abc302_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  ll a, b;
+  cin >> a >> b;
+  cout << (a + b - 1) / b << endl;
+
+  return 0;
+}
