e1fbaf56ac9fb7b215bc6414af49bf3006de1bcb
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2023-06-10 21:05:13 +0900

    [AC] abc305 abc305_a

---
+++ atcoder.jp/abc305/abc305_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  cout << (n / 5) * 5 + (n % 5 >= 3 ? 5 : 0) << endl;
+  return 0;
+}
