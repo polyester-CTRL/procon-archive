5b5a91651d7359284d6331f3ecbf1581b9e6b8ea
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2023-08-05 21:03:27 +0900

    [AC] abc313 abc313_a

---
+++ atcoder.jp/abc313/abc313_a/Main.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int x = 0;
+  for(int i = 1; i < n; i++) {
+    x = max(x, a[i] - a[0] + 1);
+  }
+  cout << x << endl;
+  return 0;
+}
